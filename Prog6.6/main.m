ff8ee4c35eb5404923d6751f307e14a6c1db08b2
admin <dangtinhtong@gmail.com>
2017-11-27 13:51:16 +0700

    first commit

--- Prog6.6/main.m
+++ Prog6.6/main.m
@@ -4,14 +4,30 @@
 //
 //  Created by admin on 11/27/17.
 //  Copyright © 2017 admin. All rights reserved.
-//
+//--------------------------------
+// Program to implement the sign function
+
 
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        // NSLog(@"Hello, World!");
+        int number, sign;
+        NSLog(@"Please type in your number");
+        scanf("%i",&number);
+        if (number<0) {
+            sign=-1;
+        }
+    
+        else if(number==0){
+            sign=0;
+        }else
+        {
+            sign=1;
+        }
+        NSLog(@"Sign = %i",sign);
     }
     return 0;
 }
